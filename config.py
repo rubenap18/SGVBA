c5708c067db28f44ca6936bbb3cf0ad7370f48b2
Ruben <itsrubenaguilar@gmail.com>
2025-12-23 17:03:46 -0800

    Added connection module, main_view module and requirements.txt file name was changed.

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Configuracion de la Base de Datos (MariaDB)
-DB_HOST = 'localhost'
+DB_HOST = '127.0.0.1'
 DB_PORT = 3306
-DB_USER = 'root'
-DB_PASSWORD = ''
+DB_USER = 'admin'
+DB_PASSWORD = 'TuContraseñaSegura'
 DB_DATABASE = 'tcn' # transportes_cuervo_negro
 DB_USE_PURE = True
 
